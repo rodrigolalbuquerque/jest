ccf2060b30d318ec14d33c7ba7ff5ed9c2fb237a
Robrigado <robrigadodev@gmail.com>
2023-08-16 16:48:39 -0300

    A358 - factory c/ parametros, toBeCloseTo() p/ floats

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,16 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should have properties name and price with values Camiseta and 49.9', () => {
+    const sut = createSut('Camiseta', 49.9);
+
+    expect(sut.name).toBe('Camiseta');
+    expect(sut.price).toBeCloseTo(49.9);
+  });
+});
